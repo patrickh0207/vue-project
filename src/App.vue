abf281087539817612181f70ed0b3b31854929dc
Patrick Hampel <patrick.hampel@memsource.com>
2022-12-01 16:50:50 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  let myVar = ref("Hello World!")
+
+  const myFunc = () => {
+    myVar.value = "I clicked a button!"
+  }
+</script>
+
+<template>
+ <h1>{{ myVar }}</h1>
+ <button @click="myFunc">click me</button>
+</template>
+
+<style scoped>
+
+</style>
